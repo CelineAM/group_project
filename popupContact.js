46fb89c9ddebf9d312d444bc91f18e457373de00
CelineAM <56314869+CelineAM@users.noreply.github.com>
2019-11-23 23:15:03 +0000

    Ready for review
    Need to add navbar

---
+++ popupContact.js
@@ -0,0 +1,17 @@
+// $("#buttonsend").click(function(event) {
+//   var text = $("#input").val();
+//   $("#list").append('<li>' + text + '</li>');
+// }
+// window.alert("Message sent");
+function SendingMessage() {
+  window.alert("Message sent, thank you.");
+}
+function TwitterLink() {
+  window.open("https://twitter.com/");
+}
+function FacebookLink() {
+  window.open("https://facebook.com/");
+}
+function PinterestLink() {
+  window.open("https://pinterest.co.uk/");
+}
