f3bf579007f9885de8a47c2aadbe2fe645c8c907
CelineAM <56314869+CelineAM@users.noreply.github.com>
2019-11-26 18:22:08 +0000

    about part updated
    conflicts to merge the about branch so copy pasted in new files instead

---
+++ about.js
@@ -0,0 +1,12 @@
+function SendingMessage() {
+  window.alert("Message sent, thank you.");
+}
+function TwitterLink() {
+  window.open("https://twitter.com/");
+}
+function FacebookLink() {
+  window.open("https://facebook.com/");
+}
+function PinterestLink() {
+  window.open("https://pinterest.co.uk/");
+}
