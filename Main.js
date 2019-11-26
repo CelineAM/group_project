f83b17cb9dd9de5edc532463a442d7448e414daa
CelineAM <56314869+CelineAM@users.noreply.github.com>
2019-11-26 00:54:08 +0000

    Sign in and Register buttons

---
+++ Main.js
@@ -0,0 +1,38 @@
+function SignInPopUp() {
+  window.prompt("Your username", "Type here")
+  window.prompt("Your password", "Type here")
+  window.alert("You are logged in")
+}
+
+function RegisterPopUp() {
+  window.prompt("Choose a username", "Type here")
+  window.prompt("Choose a password", "Type here")
+  window.alert("Welcome!")
+}
+
+
+  // var NewOrExisting;
+  // if (NewOrExisting)
+  // var Mypassword = prompt("password here","")
+  // window.prompt(MyName, Mypassword);
+
+// function SignInPopUp() {
+//   var MyName = prompt("name here","blabla")
+//   var Mypassword = prompt("password here","")
+//   window.prompt(MyName, Mypassword);
+// }
+// alert(MyName +)
+//
+// function SignInPopUp() {
+//   var Login;
+//   var Existing = confirm("Log in");
+//   if (Existing == true) {
+//     Login = window.prompt("Username", "Type it");
+//   } else {
+//     Login = window.prompt("Choose an identifiant", "Type");
+//   }
+// }
+
+// function SignInPopUp() {
+//   window.prompt("Username", "Type it" + "password", "type");
+// }
